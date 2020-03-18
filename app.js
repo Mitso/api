b80f4101001e74e11c48415c9ad368b69ded59fc
Mitso Qalaba <mqalaba@gmail.com>
2020-03-18 17:37:48 +0200

    update url configs

--- app.js
+++ app.js
@@ -46,8 +46,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', index);
-app.use('/api', api);
+app.use('/', api);
 //app.use('/users', users);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
